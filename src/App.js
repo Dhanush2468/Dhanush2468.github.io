85f3c18a906985578bd65f8cc73ba1cb46d5f36f
Dhanush2468 <dhanushgollavilli02@gmail.com>
2023-09-22 09:40:07 +0530

    linkednin

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Form from './components/Form/Form'
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Form/>
+    </div>
+  );
+}
+
+export default App;
